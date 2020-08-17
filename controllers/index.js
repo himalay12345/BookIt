2edd2ca2ac432489e6b51a7a5df232b318d09302
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2020-08-17 19:10:53 +0530

    Index Router setup

---
+++ controllers/index.js
@@ -0,0 +1,232 @@
+
+module.exports.home = (req, res)=>{
+    return res.render('index',{
+        title:'Home'
+    })
+}
+
+module.exports.addBilling = (req, res)=>{
+    return res.render('add-billing',{
+        title:'Add Billing'
+    })
+}
+module.exports.addPrescription = (req, res)=>{
+    return res.render('add-prescription',{
+        title:'Add Prescription'
+    })
+}
+
+module.exports.appointments = (req, res)=>{
+    return res.render('appointments',{
+        title:'Appointments'
+    })
+}
+
+module.exports.blankPage = (req, res)=>{
+    return res.render('blank-page',{
+        title:'Blank'
+    })
+}
+
+module.exports.booking = (req, res)=>{
+    return res.render('booking',{
+        title:'Booking'
+    })
+}
+
+module.exports.bookingSuccess = (req, res)=>{
+    return res.render('booking-success',{
+        title:'Booking-Success'
+    })
+}
+
+module.exports.calendar = (req, res)=>{
+    return res.render('calendar',{
+        title:'Calendar'
+    })
+}
+
+module.exports.changePassword = (req, res)=>{
+    return res.render('change-password',{
+        title:'Change Password'
+    })
+}
+
+module.exports.chat = (req, res)=>{
+    return res.render('chat',{
+        title:'Chat'
+    })
+}
+
+module.exports.chatDoctor = (req, res)=>{
+    return res.render('chat-doctor',{
+        title:'Chat Doctor'
+    })
+}
+
+module.exports.checkout = (req, res)=>{
+    return res.render('checkout',{
+        title:'Checkout'
+    })
+}
+module.exports.components= (req, res)=>{
+    return res.render('components',{
+        title:'Components'
+    })
+}
+
+module.exports.doctorChangePassword = (req, res)=>{
+    return res.render('doctor-change-password',{
+        title:'Change Password'
+    })
+}
+
+module.exports.doctorDashboard = (req, res)=>{
+    return res.render('doctor-dashboard',{
+        title:'Doctor Dashboard'
+    })
+}
+
+module.exports.doctorProfile = (req, res)=>{
+    return res.render('doctor-profile',{
+        title:'Profile'
+    })
+}
+
+module.exports.doctorProfileSettings = (req, res)=>{
+    return res.render('doctor-profile-settings',{
+        title:'Profile Settings'
+    })
+}
+
+module.exports.doctorRegister = (req, res)=>{
+    return res.render('doctor-register',{
+        title:'Register'
+    })
+}
+
+module.exports.editBilling = (req, res)=>{
+    return res.render('edit-billing',{
+        title:'Edit Billing'
+    })
+}
+
+module.exports.editPrescription = (req, res)=>{
+    return res.render('edit-prescription',{
+        title:'Edit prescription'
+    })
+}
+
+module.exports.favourites = (req, res)=>{
+    return res.render('favourites',{
+        title:'Favourites'
+    })
+}
+
+module.exports.forgotPassword = (req, res)=>{
+    return res.render('forgot-password',{
+        title:'Forgot Password'
+    })
+}
+
+module.exports.home2 = (req, res)=>{
+    return res.render('index-2',{
+        title:'Home'
+    })
+}
+
+module.exports.invoiceView = (req, res)=>{
+    return res.render('invoice-view',{
+        title:'Invoice View'
+    })
+}
+
+module.exports.invoices = (req, res)=>{
+    return res.render('invoices',{
+        title:'Invoices'
+    })
+}
+
+module.exports.login = (req, res)=>{
+    return res.render('login',{
+        title:'Login'
+    })
+}
+
+module.exports.myPatients = (req, res)=>{
+    return res.render('my-patients',{
+        title:'My Patients'
+    })
+}
+
+module.exports.patientDashboard= (req, res)=>{
+    return res.render('patient-dashboard',{
+        title:'Patient Dashboard'
+    })
+}
+
+module.exports.patientProfile = (req, res)=>{
+    return res.render('patient-profile',{
+        title:'Patient Profile'
+    })
+}
+
+module.exports.privacyPolicy = (req, res)=>{
+    return res.render('privacy-policy',{
+        title:'Privacy Policy'
+    })
+}
+
+module.exports.profileSettings = (req, res)=>{
+    return res.render('profile-settings',{
+        title:'Profile Settings'
+    })
+}
+
+module.exports.register = (req, res)=>{
+    return res.render('register',{
+        title:'Register'
+    })
+}
+
+module.exports.reviews = (req, res)=>{
+    return res.render('reviews',{
+        title:'Reviews'
+    })
+}
+
+module.exports.scheduleTimings = (req, res)=>{
+    return res.render('schedule-timings',{
+        title:'Schedule Timings'
+    })
+}
+
+module.exports.search = (req, res)=>{
+    return res.render('search',{
+        title:'Search'
+    })
+}
+
+module.exports.socialMedia = (req, res)=>{
+    return res.render('social-media',{
+        title:'Social Media'
+    })
+}
+
+module.exports.termCondition = (req, res)=>{
+    return res.render('term-condition',{
+        title:'Term Condition'
+    })
+}
+
+module.exports.videoCall = (req, res)=>{
+    return res.render('video-call',{
+        title:'Video Call'
+    })
+}
+
+module.exports.voiceCall = (req, res)=>{
+    return res.render('voice-call',{
+        title:'Voice Call'
+    })
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.use(express.static('./assets'));
+app.use('/',require('./routes/index'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log('Error',err);
+        return;    
+    }
+
+    console.log('Server is running on port ',port);
+})
