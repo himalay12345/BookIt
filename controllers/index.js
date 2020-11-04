f2b2160c0cfd9132e56bd4ed10da6f80ca9e5197
“hymnsbharat234” <chandrakantyadav814101@gmail.com>
2020-11-04 11:27:53 +0530

    upadated

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ const Consult = require('../models/consult');
 const shortid = require('shortid');
 const Razorpay = require('razorpay');
 const emailVerification = require('../mailers/email-otp');
-const Consult = require('../models/consult');
+
 
 
 
@@ -48,11 +48,11 @@ module.exports.addBilling = (req, res) => {
 module.exports.consult = async(req, res) => {
     let doctors = await User.find({ type: "Doctor" });
     let consults = await Consult.find({});
- 
+
     return res.render('consult', {
         title: 'Consult',
         doctors: doctors,
-        consults:consults
+        consults: consults
     })
 }
 module.exports.addPrescription = (req, res) => {
@@ -1022,10 +1022,8 @@ module.exports.staffBooking = async(req, res) => {
         }
     }
     var todayd = new Date();
-    for(temp of user1.tracked)
-    {
-        if(todayd > temp.createdAt)
-        {
+    for (temp of user1.tracked) {
+        if (todayd > temp.createdAt) {
             user1.tracked.pull(temp._id);
         }
     }
