704e1be2688ce40e36680445058de2df6610723d
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2020-12-08 10:18:31 +0530

    updated

--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ const development = {
     fb_profile_fields: ['id', 'displayName', 'picture.type(large)', 'email'],
     twilio_sid: "VA8bd100c88cde205a490376c7a1a6273a",
     twilio_account_sid: "AC072f252c5635d5be8d199882a12ea742",
-    twilio_auth_token: "2ee8a1e3e1c8f8294b4d106ca7c8065d",
+    twilio_auth_token: "39f0cd3e7017d6a00ed51d6b3cb36f8a",
     gmail_passw: 'Himalay@NIXXIT',
     morgan: {
         mode: 'dev',
