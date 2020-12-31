6b6d2ff16998b5c4d0bfa8e26f7be2515900512f
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2020-12-31 17:51:01 +0530

    updated

--- config/environment.js
+++ config/environment.js
@@ -98,5 +98,5 @@ const production = {
 }
 
 
-module.exports = eval(process.env.AAROGYAHUB_ENVIRONMENT == undefined) ? development : eval(process.env.AAROGYAHUB_ENVIRONMENT);
-// module.exports = development;
+// module.exports = eval(process.env.AAROGYAHUB_ENVIRONMENT == undefined) ? development : eval(process.env.AAROGYAHUB_ENVIRONMENT);
+module.exports = development;
