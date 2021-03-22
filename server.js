3fa472dd5f021cd4b9045f213330c711fb8d8f79
“hymnsbharat234” <chandrakantyadav814101@gmail.com>
2021-03-22 09:08:08 +0530

    updated

--- server.js
+++ server.js
@@ -24,34 +24,34 @@ const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 const cors = require('cors');
 
-// const trackServer = require('http').Server(app);
-// const trackSockets = require('./config/track_socket').trackSockets(trackServer);
-// trackServer.listen(5000);
+const trackServer = require('http').Server(app);
+const trackSockets = require('./config/track_socket').trackSockets(trackServer);
+trackServer.listen(5000);
 
-const fs = require('fs');
-const https = require('https');
-var secureServer = https.createServer({ key: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/chain.pem'),
-    requestCert: false,     
-    rejectUnauthorized: false },app);
-const trackSockets = require('./config/track_socket').trackSockets(secureServer);
-secureServer.listen(5000);   
+// const fs = require('fs');
+// const https = require('https');
+// var secureServer = https.createServer({ key: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/cert.pem'),
+//     ca: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/chain.pem'),
+//     requestCert: false,     
+//     rejectUnauthorized: false },app);
+// const trackSockets = require('./config/track_socket').trackSockets(secureServer);
+// secureServer.listen(5000);   
 
 
 const sassMiddleware = require('node-sass-middleware');
 console.log(env.name);
 console.log(env.asset_path);
 
-if(env.name == 'development'){
+if (env.name == 'development') {
 
-app.use(sassMiddleware({
-    src:path.join(__dirname,env.asset_path,'scss'),
-    dest:path.join(__dirname,env.asset_path,'css'),
-    debug: true,
-    outputStyle:'extended',
-    prefix:'/css'
-}));
+    app.use(sassMiddleware({
+        src: path.join(__dirname, env.asset_path, 'scss'),
+        dest: path.join(__dirname, env.asset_path, 'css'),
+        debug: true,
+        outputStyle: 'extended',
+        prefix: '/css'
+    }));
 }
 console.log('Patient Tracking server is running on port 5000');
 
@@ -113,22 +113,22 @@ app.use(customMiddleware.setFlash);
 app.use('/', require('./routes/index'));
 
 const swaggerOptions = {
-    swaggerDefinition:{
-        info:{
-            title:'Home Page Api',
-            version:'1.0.0'
+    swaggerDefinition: {
+        info: {
+            title: 'Home Page Api',
+            version: '1.0.0'
         }
     },
-    apis:['./routes/api.js']
+    apis: ['./routes/api.js']
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // console.log(swaggerDocs);
 var options2 = {
     explorer: true
-  };
+};
 
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs,options2))
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs, options2))
 
 app.listen(port, function(err) {
     if (err) {
