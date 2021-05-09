f019199669c4d7e0b1af1a2ca5cf7c6292ba4ab9
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2021-05-09 13:58:02 +0530

    updated

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ console.log('Patient Tracking server is running on port 5000');
 // }))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Origin', 'https://aarogyahub.netlify.app')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Credentials', true)
