bdc31bf0789f09a3c7da55287d7ab6cb6af77917
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2021-05-23 16:28:53 +0530

    updated

--- config/twilio.js
+++ config/twilio.js
@@ -1,6 +1,6 @@
 const env = require('./environment');
 module.exports = {
-    serviceID: 'VA93aa40f9ee628352fb6022cfeb1cbd5f',
+    serviceID: env.twilio_sid,
     accountSID: env.twilio_account_sid,
     authToken: env.twilio_auth_token
 
