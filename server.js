06c5a10a8c002efa8fb0d6ba43383775bc970c5f
himalay12345 <47078695+himalay12345@users.noreply.github.com>
2021-05-26 09:16:27 +0530

    updatd

--- server.js
+++ server.js
@@ -24,19 +24,19 @@ const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 const cors = require('cors');
 
-// const trackServer = require('http').Server(app);
-// const trackSockets = require('./config/track_socket').trackSockets(trackServer);
-// trackServer.listen(5000);
+const trackServer = require('http').Server(app);
+const trackSockets = require('./config/track_socket').trackSockets(trackServer);
+trackServer.listen(5000);
 
-const fs = require('fs');
-const https = require('https');
-var secureServer = https.createServer({ key: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/chain.pem'),
-    requestCert: false,     
-    rejectUnauthorized: false },app);
-const trackSockets = require('./config/track_socket').trackSockets(secureServer);
-secureServer.listen(5000);   
+// const fs = require('fs');
+// const https = require('https');
+// var secureServer = https.createServer({ key: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/cert.pem'),
+//     ca: fs.readFileSync('/etc/letsencrypt/live/aarogyahub.com/chain.pem'),
+//     requestCert: false,     
+//     rejectUnauthorized: false },app);
+// const trackSockets = require('./config/track_socket').trackSockets(secureServer);
+// secureServer.listen(5000);   
 
 
 const sassMiddleware = require('node-sass-middleware');
